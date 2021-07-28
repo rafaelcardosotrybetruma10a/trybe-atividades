6103b6067d780769fc1983a7fd2181119440fa92
Rafael Cardoso <rafael.vimur@gmail.com>
2021-07-28 02:01:23 -0300

    adiciona o pagina de login

--- front-end/pratica_redux/src/App.js
+++ front-end/pratica_redux/src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import store from './redux/index';
 import Home from './pages/Home';
+import Login from './pages/Login'
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
           </Switch>
         </BrowserRouter>
       </Provider>
