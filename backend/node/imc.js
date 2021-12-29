a47275524c5c390107c883693be1fd1aa563059b
Rafael Cardoso <rafael.vimur@gmail.com>
2021-12-29 15:56:02 -0300

    adicionando redline

--- backend/node/imc.js
+++ backend/node/imc.js
@@ -1,15 +1,38 @@
-const PESO_94KG = 94;
-const ALTURA_1M_78CM = 178;
+const readline = require('readline-sync');
 
 function calculaImc () {
-  const peso = PESO_94KG;
-  const altura = ALTURA_1M_78CM;
-
-  console.log(`Peso: ${peso}, Altura: ${altura}`);
+	const peso = readline.questionInt('Qual o seu peso? (em kg)');
+  const altura = readline.questionInt('Qual a sua altura? (em cm)');
 
   const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
 
-  console.log(`IMC: ${imc}`);
+  // console.log(`IMC: ${imc}`);
+	if (imc < 18.5) {
+		console.log('Situação: Abaixo do peso (magreza)');
+		return;
+	}
+	
+	if (imc >= 18.5 && imc < 25) {
+		console.log('Situação: Peso normal');
+		return;
+	}
+	
+	if (imc >= 25 && imc < 30) {
+		console.log('Situação: Acima do peso (sobrepeso)');
+		return;
+	}
+	
+	if (imc >= 30 && imc < 35) {
+		console.log('Situação: Obesidade grau I');
+		return;
+	}
+	
+	if (imc >= 35 && imc < 40) {
+		console.log('Situação: Obesidade grau II');
+		return;
+	}
+	
+	console.log('Situação: Obesidade graus III e IV');
 }
 
 calculaImc();
